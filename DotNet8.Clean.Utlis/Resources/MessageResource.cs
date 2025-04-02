2af912326c03e2be8de9b9c8ad816242a36c19dc
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-02 20:40:10 +0630

    -

--- DotNet8.Clean.Utlis/Resources/MessageResource.cs
+++ DotNet8.Clean.Utlis/Resources/MessageResource.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Clean.Utlis.Resources
+﻿namespace DotNet8.Clean.Utlis.Resources
 {
 	internal class MessageResource
 	{
