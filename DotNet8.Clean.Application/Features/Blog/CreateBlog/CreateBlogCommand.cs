5dca321a7a6ee7dd82604c664960d3cb798b1db6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-02 22:22:07 +0630

    Format Code

--- DotNet8.Clean.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
+++ DotNet8.Clean.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.Clean.Application.Features.Blog.CreateBlog
+﻿namespace DotNet8.Clean.Application.Features.Blog.CreateBlog;
+
+internal class CreateBlogCommand
 {
-	internal class CreateBlogCommand
-	{
-	}
 }
