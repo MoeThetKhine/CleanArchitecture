8e128d412648d326d0425ad725ca5c10ffb5a8ae
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-02 20:45:08 +0630

    Feat: Add Result

--- DotNet8.Clean.Utlis/Result.cs
+++ DotNet8.Clean.Utlis/Result.cs
@@ -1,5 +1,11 @@
-﻿namespace DotNet8.Clean.Utlis;
+﻿using DotNet8.Clean.Utlis.Enums;
+
+namespace DotNet8.Clean.Utlis;
 
 public class Result<T>
 {
+	public T Data { get; set; }
+	public string Message { get; set; }
+	public bool IsSuccess {  get; set; }
+	public EnumStatusCode StatusCode { get; set; }
 }
