48517e48b3f7ad31b3eb74dda1dc3bbe1d3a8692
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-02 16:04:31 +0630

    Feat : Add Global Using

--- DotNet8.Clean.DbService/AppDbContextModels/AppDbContext.cs
+++ DotNet8.Clean.DbService/AppDbContextModels/AppDbContext.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace DotNet8.Clean.DbService.AppDbContextModels;
+﻿namespace DotNet8.Clean.DbService.AppDbContextModels;
 
 public partial class AppDbContext : DbContext
 {
