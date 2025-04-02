04a373ea21ef59ea6d0a8a216f0ab9727d4dcb97
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-02 22:16:45 +0630

    Format Code

--- DotNet8.Clean.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
+++ DotNet8.Clean.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.Clean.Application.Features.Blog.GetBlogList
+﻿namespace DotNet8.Clean.Application.Features.Blog.GetBlogList;
+
+internal class GetBlogListQuery
 {
-	internal class GetBlogListQuery
-	{
-	}
 }
