3ceaa907bd6abe805327ceef8ed1b69d16e2306d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-02 18:48:02 +0630

    -

--- DotNet8.Clean.DTO/Features/Blog/BlogListModel.cs
+++ DotNet8.Clean.DTO/Features/Blog/BlogListModel.cs
@@ -1,10 +1,14 @@
 ﻿namespace DotNet8.Clean.DTO.Features.Blog;
 
+#region BlogListModel
+
 public class BlogListModel
 {
 	public IEnumerable<BlogModel> DataLst { get; set; }
 	public PageSettingModel PageSetting { get; set; }
 }
+
+#endregion
 
 public class BlogListModelV1
 {
