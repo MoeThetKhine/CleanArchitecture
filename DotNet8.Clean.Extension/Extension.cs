b660b4a044b80524d52f0537cee32ac0790a9a03
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-02 20:24:47 +0630

    Format Code

--- DotNet8.Clean.Extension/Extension.cs
+++ DotNet8.Clean.Extension/Extension.cs
@@ -1,35 +1,34 @@
-﻿namespace DotNet8.Clean.Extension
+﻿namespace DotNet8.Clean.Extension;
+
+public static class Extension
 {
-	public static class Extension
+	#region ToModel
+
+	public static BlogModel ToModel(this TblBlog dataModel)
 	{
-		#region ToModel
-
-		public static BlogModel ToModel(this TblBlog dataModel)
+		return new BlogModel
 		{
-			return new BlogModel
-			{
-				BlogId = dataModel.BlogId,
-				BlogTitle = dataModel.BlogTitle,
-				BlogAuthor = dataModel.BlogAuthor,
-				BlogContent = dataModel.BlogContent,
-				DeleteFlag = dataModel.DeleteFlag
-			};
-		}
-
-		#endregion
-
-		#region ToEntity
-
-		public static TblBlog ToEntity(this BlogRequestModel model)
-		{
-			return new TblBlog
-			{
-				BlogTitle = model.BlogTitle,
-				BlogAuthor = model.BlogAuthor,
-				BlogContent = model.BlogContent
-			};
-		}
-
-		#endregion
+			BlogId = dataModel.BlogId,
+			BlogTitle = dataModel.BlogTitle,
+			BlogAuthor = dataModel.BlogAuthor,
+			BlogContent = dataModel.BlogContent,
+			DeleteFlag = dataModel.DeleteFlag
+		};
 	}
+
+	#endregion
+
+	#region ToEntity
+
+	public static TblBlog ToEntity(this BlogRequestModel model)
+	{
+		return new TblBlog
+		{
+			BlogTitle = model.BlogTitle,
+			BlogAuthor = model.BlogAuthor,
+			BlogContent = model.BlogContent
+		};
+	}
+
+	#endregion
 }
