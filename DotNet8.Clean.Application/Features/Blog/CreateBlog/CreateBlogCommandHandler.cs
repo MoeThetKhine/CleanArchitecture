5c2b47d124da4c053d54a3afbe7dc52e51d32fb8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-03 12:19:08 +0630

    -

--- DotNet8.Clean.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
+++ DotNet8.Clean.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
@@ -9,6 +9,8 @@ public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, Resul
 		_blogRepository = blogRepository;
 	}
 
+	#region Handle
+
 	public async Task<Result<BlogModel>> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
 	{
 		Result<BlogModel> result;
@@ -43,4 +45,6 @@ public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, Resul
 		result:
 		return result;
 	}
+
+	#endregion
 }
