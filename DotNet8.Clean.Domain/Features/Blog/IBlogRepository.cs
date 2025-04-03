4ad7c0509c1be4875a4bf8ae8047ae72343258e0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-03 10:13:24 +0630

    Feat : Add CreateBlogAsync in IBlogRepository

--- DotNet8.Clean.Domain/Features/Blog/IBlogRepository.cs
+++ DotNet8.Clean.Domain/Features/Blog/IBlogRepository.cs
@@ -1,5 +1,9 @@
-﻿namespace DotNet8.Clean.Domain.Features.Blog;
+﻿using DotNet8.Clean.DTO.Features.Blog;
+using DotNet8.Clean.Utlis;
+
+namespace DotNet8.Clean.Domain.Features.Blog;
 
 public interface IBlogRepository
 {
+	Task<Result<BlogModel>> CreateBlogAsync(BlogRequestModel blogRequest, CancellationToken cancellationToken);
 }
