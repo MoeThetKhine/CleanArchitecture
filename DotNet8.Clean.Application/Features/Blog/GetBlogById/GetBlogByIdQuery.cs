f298ac4896c1123d0c0cba8ac72a0ab8338e9dce
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-03 18:39:29 +0630

    Feat : Add GetBlogByIdQuery

--- DotNet8.Clean.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
+++ DotNet8.Clean.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
@@ -1,5 +1,11 @@
 ﻿namespace DotNet8.Clean.Application.Features.Blog.GetBlogById;
 
-public class GetBlogByIdQuery
+public class GetBlogByIdQuery : IRequest<Result<BlogModel>>
 {
+	public int BlogId { get; set; }
+
+	public GetBlogByIdQuery(int blogId)
+	{
+		BlogId = blogId;
+	}
 }
