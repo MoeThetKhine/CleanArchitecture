33ea1f9148890b66b44284e9cf81be7a0ed67f09
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-03 16:51:50 +0630

    -

--- DotNet8.Clean.Infrastructure/Features/Blog/BlogRepository.cs
+++ DotNet8.Clean.Infrastructure/Features/Blog/BlogRepository.cs
@@ -205,6 +205,8 @@ public class BlogRepository : IBlogRepository
 
 	#endregion
 
+	#region DeleteBlogAsync
+
 	public async Task<Result<BlogModel>> DeleteBlogAsync(int id, CancellationToken cancellationToken)
 	{
 		Result<BlogModel> result;
@@ -231,4 +233,6 @@ public class BlogRepository : IBlogRepository
 	result:
 		return result;
 	}
+
+	#endregion
 }
