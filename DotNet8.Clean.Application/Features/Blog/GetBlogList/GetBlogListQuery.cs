0d930b17ebab5d8593dd8f64f39d6e7d64960a18
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-03 13:24:59 +0630

    Feat : Add GetBlogListQuery

--- DotNet8.Clean.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
+++ DotNet8.Clean.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
@@ -1,5 +1,13 @@
 ﻿namespace DotNet8.Clean.Application.Features.Blog.GetBlogList;
 
-public class GetBlogListQuery
+public class GetBlogListQuery : IRequest<Result<BlogListModelV1>>
 {
+	public int PageNo {  get; set; }
+	public int PageSize {  get; set; }
+
+	public GetBlogListQuery(int pageNo, int pageSize)
+	{
+		PageNo = pageNo;
+		PageSize = pageSize;
+	}
 }
