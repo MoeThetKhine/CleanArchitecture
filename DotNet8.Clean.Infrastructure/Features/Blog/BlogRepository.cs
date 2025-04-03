0b65fd9e738125dcd2b00c7de2a0a51d81336beb
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-03 15:02:52 +0630

    Feat : Add GetBlogsAsync  in BlogRepository

--- DotNet8.Clean.Infrastructure/Features/Blog/BlogRepository.cs
+++ DotNet8.Clean.Infrastructure/Features/Blog/BlogRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace DotNet8.Clean.Infrastructure.Features.Blog;
+﻿using DotNet8.Clean.DTO.Features.PageSetting;
+using DotNet8.Clean.Shared;
+
+namespace DotNet8.Clean.Infrastructure.Features.Blog;
 
 public class BlogRepository : IBlogRepository
 {
@@ -7,6 +10,50 @@ public class BlogRepository : IBlogRepository
 	public BlogRepository(BlogDbContext context)
 	{
 		_context = context;
+	}
+
+	public async Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
+	{
+		Result<BlogListModelV1> result;
+
+		try
+		{
+			var query = _context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
+			var lst = await query
+				.Paginate(pageNo, pageSize)
+				.ToListAsync(cancellationToken: cancellationToken);
+
+			var totalCount = await query.CountAsync(cancellationToken: cancellationToken);
+			var pageCount = totalCount / pageSize;
+
+			if(totalCount % pageSize > 0)
+			{
+				pageCount++;
+			}
+
+			var pageSettingModel = new PageSettingModel(pageNo, pageSize, pageCount, totalCount);
+
+			var model = new BlogListModelV1()
+			{
+				DataLst = lst.Select(x => new BlogModel()
+				{
+					BlogId = x.BlogId,
+					BlogTitle = x.BlogTitle,
+					BlogAuthor = x.BlogAuthor,
+					BlogContent = x.BlogContent,
+				}).AsQueryable(),
+				PageSetting = pageSettingModel
+			};
+
+			result = Result<BlogListModelV1>.Success(model);
+
+		}
+		catch (Exception ex)
+		{
+			result = Result<BlogListModelV1>.Failure(ex);
+		}
+		result:
+		return result;
 	}
 
 	#region CreateBlogAsync
