7a34a612513c45a9a67a941da80e94f460bf1ccc
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-03 20:30:59 +0630

    -

--- DotNet8.Clean.Application/Features/Blog/GetBlogById/GetBlogByIdQueryHandler.cs
+++ DotNet8.Clean.Application/Features/Blog/GetBlogById/GetBlogByIdQueryHandler.cs
@@ -1,34 +1,12 @@
-﻿
-namespace DotNet8.Clean.Application.Features.Blog.GetBlogById;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-public class GetBlogByIdQueryHandler : IRequestHandler<GetBlogByIdQuery, Result<BlogModel>>
+namespace DotNet8.Clean.Application.Features.Blog.GetBlogById
 {
-	private readonly IBlogRepository _blogRepository;
-
-	public GetBlogByIdQueryHandler(IBlogRepository blogRepository)
+	internal class GetBlogByIdQueryHandler
 	{
-		_blogRepository = blogRepository;
 	}
-
-	#region Handle
-
-	public async Task<Result<BlogModel>> Handle(GetBlogByIdQuery request, CancellationToken cancellationToken)
-	{
-		Result<BlogModel> result;
-
-		if (request.BlogId <= 0)
-		{
-			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
-			goto result;
-		}
-
-		result = await _blogRepository.GetBlogByIdAsync(request.BlogId, cancellationToken);
-
-	result:
-		return result;
-	}
-
-	#endregion
-
 }
