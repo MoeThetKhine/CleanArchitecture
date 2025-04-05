f8db2e8f43b456cb09666966636f5597b36822ae
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 15:10:34 +0630

    Feat : Add Content

--- DotNet8.Clean.Presentation/Controllers/BaseController.cs
+++ DotNet8.Clean.Presentation/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNet8.Clean.Shared;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8.Clean.Presentation.Controllers;
 
@@ -6,4 +7,8 @@ namespace DotNet8.Clean.Presentation.Controllers;
 [ApiController]
 public class BaseController : ControllerBase
 {
+	protected IActionResult Content(object obj)
+	{
+		return Content(obj.ToJson(), "application/json");
+	}
 }
