2cc46e51484402bada5f37ade04ce91d5ce34925
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-05 15:13:51 +0630

    Feat : Add GetBlogAsync in Controller

--- DotNet8.Clean.Presentation/Controllers/Blog/BlogController.cs
+++ DotNet8.Clean.Presentation/Controllers/Blog/BlogController.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNet8.Clean.Application.Features.Blog.GetBlogList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8.Clean.Presentation.Controllers.Blog;
 
 [Route("api/[controller]")]
 [ApiController]
-public class BlogController : ControllerBase
+public class BlogController : BaseController
 {
+	private readonly IMediator _mediator;
+
+	public BlogController(IMediator mediator)
+	{
+		_mediator = mediator;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetBlogAsync(int pageNo, int  pageSize, CancellationToken cancellationToken)
+	{
+		var query = new GetBlogListQuery(pageNo, pageSize);
+		var result = await _mediator.Send(query,cancellationToken);
+
+		return Content(result);
+	}
+
 }
